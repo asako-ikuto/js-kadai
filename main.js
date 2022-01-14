08d24330b8ac2ccb63b64bacd8785ae2851d6215
ikuto-asako <m131932@gmail.com>
2022-01-15 07:15:48 +0900

    課題1-3_20220115

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 'use strict';
 
-const question = '日本の首都は？';
-let answer = prompt(question);
+const taskTitle = `============================
+現在持っているタスクの一覧
+============================`;
 
-//answerが'東京'になるまで繰り返す
-while (answer !== '東京') {
-    alert('不正解です');
-    answer = prompt(question);
+//タスク初期値
+const tasks = new Array('掃除', '買い物', '散歩');
+
+//タスク一覧を表示する
+const taskIndex = () => {
+    console.log(taskTitle);
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(`${i} : ${tasks[i]}`);
+    }
 }
 
-alert('正解です');
+//タスクを追加する
+const addTask = () => {
+    //操作内容確認
+    let confOpe = prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
+    //タスク入力・タスク一覧に追加
+    let newTask = prompt('タスクを入力してください');
+    tasks.push(newTask);
+    alert('新しいタスクを追加しました');
+
+    taskIndex();
+    addTask();
+}
+
+//1回目の処理
+taskIndex();
+addTask();
