47182de629804a5f4826e5e28806b87bc0583a7f
ikuto-asako <m131932@gmail.com>
2022-01-16 07:29:31 +0900

    課題1-3修正前_20220115

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
-for (let i = 1; i < 100; i++) {
-    //3の倍数かつ5の倍数ならFizzBuzzと表示
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz' + " " + i);
-        //3の倍数ならFizzと表示
-    } else if (i % 3 == 0) {
-        console.log('Fizz' + " " + i);
-        //5の倍数ならBuzzと表示
-    } else if (i % 5 == 0) {
-        console.log('Buzz' + " " + i);
+'use strict';
+
+const taskTitle = `============================
+現在持っているタスクの一覧
+============================`;
+
+//タスク初期値
+const tasks = newArray('掃除', '買い物', '散歩');
+
+//タスク一覧を表示する
+const taskIndex = () => {
+    console.log(taskTitle);
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(`${i} : ${tasks[i]}`);
     }
-}
+};
+
+//タスクを追加する
+const addTask = () => {
+    //操作内容確認
+    let confOpe = prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
+    //タスク入力・タスク一覧に追加
+    let newTask = prompt('タスクを入力してください');
+    tasks.push(newTask);
+    alert('新しいタスクを追加しました');
+
+    taskIndex();
+    addTask();
+};
+
+//1回目の処理
+taskIndex();
+addTask();
